063b44621d2b085b4edb61719dc95511601812a1
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-04 16:22:46 -0600

    feat: implement authentication and role-based navigation in CompleteProfile component

--- src/features/carriers/presentation/screens/CompleteProfile.tsx
+++ src/features/carriers/presentation/screens/CompleteProfile.tsx
@@ -1,3 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/config/config";
+
 export function CompleteProfile() {
+  const isSignedIn = useSelector((state: RootState) => state.auth.isSignedIn);
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  if (!isSignedIn) return <Navigate to={'/login'} replace />
+
+  if (user?.role !== "carrier" || user.carrierId !== null) {
+    return <Navigate to={'/dashboard'} replace />
+  }
+
   return <div />;
 }
