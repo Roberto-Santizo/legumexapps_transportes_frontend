64db574f1a2516fc6c8aa474a6bcbb54098756ca
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-19 16:31:16 -0600

    feat: add route for creating trips in the app router

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import { CreateLocation, IndexLocations, ShowLocation, UpdateLocation } from "@/
 import { CreateProduct, IndexProducts, ShowProduct, UpdateProduct } from "@/features/products/products";
 import { CreateVehicle, IndexVehicles, ShowVehicle, UpdateVehicle } from "@/features/vehicles/vehicles";
 import { CreateZone, IndexZones, ShowZone, UpdateZone } from "@/features/zones/zones";
+import { CreateTrip } from "@/features/trips/trips";
 import { IndexPilots } from "@/features/pilots/pilots";
 import { Dashboard } from "@/features/dashboard/dashboard";
 import { Profile, ProtectedLayout, PublicLayout } from "@/features/shared/shared";
@@ -65,6 +66,10 @@ export default function AppRouter() {
 
                 <Route element={<ProtectedLayout />}>
                     <Route path="/pilotos" element={<IndexPilots />} />
+                </Route>
+
+                <Route element={<ProtectedLayout />}>
+                    <Route path="/viajes/crear" element={<CreateTrip />} />
                 </Route>
             </Routes>
         </BrowserRouter>
